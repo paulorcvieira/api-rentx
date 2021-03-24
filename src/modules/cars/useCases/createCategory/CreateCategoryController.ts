08ed587c3bc117214fe54ebede9fadf19f899adb
Paulo Vieira <paullocostta86@gmail.com>
2021-03-24 17:27:40 -0300

    refactor: implements tsyringe on CreateCategoryController

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -1,15 +1,16 @@
 import { Request, Response } from 'express'
+import { container } from 'tsyringe'
 
 import { CreateCategoryUseCase } from './CreateCategoryUseCase'
 
 class CreateCategoryController {
-  constructor(private createCategoriesUseCase: CreateCategoryUseCase) {}
-
   public async handle(request: Request, response: Response): Promise<Response> {
     try {
       const { name, description } = request.body
 
-      const category = this.createCategoriesUseCase.execute({
+      const createCategoryUseCase = container.resolve(CreateCategoryUseCase)
+
+      const category = createCategoryUseCase.execute({
         name,
         description,
       })
