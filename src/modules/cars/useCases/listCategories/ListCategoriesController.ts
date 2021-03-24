df77926a2a6d22dd301bea9983321575b3f74e5c
Paulo Vieira <paullocostta86@gmail.com>
2021-03-24 17:48:14 -0300

    refactor: implements tsyringe in listCategories

--- src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from 'express'
+import { container } from 'tsyringe'
 
 import { ListCategoriesUseCase } from './ListCategoriesUseCase'
 
 class ListCategoriesController {
-  constructor(private listCategoriesUseCase: ListCategoriesUseCase) {}
-
   public async handle(request: Request, response: Response): Promise<Response> {
-    const categories = await this.listCategoriesUseCase.execute()
+    const listCategoriesUseCase = container.resolve(ListCategoriesUseCase)
+
+    const categories = await listCategoriesUseCase.execute()
 
     return response.json(categories)
   }
