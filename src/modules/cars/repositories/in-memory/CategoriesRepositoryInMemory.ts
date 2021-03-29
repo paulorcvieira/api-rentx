ea9500746a82d318c6bf29ab9d7b974d2969e1f4
Paulo Vieira <paullocostta86@gmail.com>
2021-03-29 16:49:23 -0300

    :heavy_plus_sign: feat: create CategoriesRepositoryInMemory

---
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,25 @@
+import { ICreateCategoryDTO } from '../../dtos/ICategoryDTO'
+import { Category } from '../../entities/Category'
+import { ICategoriesRepository } from '../ICategoriesRepository'
+
+class CategoriesRepositoryInMemory implements ICategoriesRepository {
+  categories: Category[] = []
+
+  async create({ name, description }: ICreateCategoryDTO): Promise<Category> {
+    const category = new Category()
+    Object.assign(category, { name, description })
+    this.categories.push(category)
+    return category
+  }
+
+  async list(): Promise<Category[]> {
+    return this.categories
+  }
+
+  async findByName(name: string): Promise<Category | undefined> {
+    const category = this.categories.find(category => category.name === name)
+    return category
+  }
+}
+
+export { CategoriesRepositoryInMemory }
