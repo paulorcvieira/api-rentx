aee5df292bc9b65be471fcfa1b575fecd0466cd1
Paulo Vieira <paullocostta86@gmail.com>
2021-03-31 09:01:07 -0300

    :hammer: feat: migration CreateCars

---
+++ src/shared/infra/typeorm/migrations/1617188949866-CreateCars.ts
@@ -0,0 +1,65 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+import { idColumn, timestampColumns } from '../utils'
+
+export class CreateCars1617188949866 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'cars',
+        columns: [
+          idColumn,
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+          {
+            name: 'daily_rate',
+            type: 'numeric',
+          },
+          {
+            name: 'available',
+            type: 'boolean',
+            default: true,
+          },
+          {
+            name: 'license_plate',
+            type: 'varchar',
+          },
+          {
+            name: 'fine_amount',
+            type: 'numeric',
+          },
+          {
+            name: 'brand',
+            type: 'varchar',
+          },
+          {
+            name: 'category_id',
+            type: 'uuid',
+            isNullable: true,
+          },
+          ...timestampColumns,
+        ],
+        foreignKeys: [
+          {
+            name: 'fk_category_car',
+            referencedTableName: 'categories',
+            referencedColumnNames: ['id'],
+            columnNames: ['category_id'],
+            onDelete: 'SET NULL',
+            onUpdate: 'SET NULL',
+          },
+        ],
+      }),
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('cars')
+  }
+}
