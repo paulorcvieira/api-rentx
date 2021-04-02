81d5986c65841f89faa5a9f51a17578b1cde7968
Paulo Vieira <paullocostta86@gmail.com>
2021-04-02 01:22:27 -0300

    :bug: fix: create category before create car

--- src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -1,15 +1,31 @@
+import { Category } from '@modules/cars/infra/typeorm/entities/Category'
 import { CarsRepositoryInMemory } from '@modules/cars/repositories/in-memory/CarsRepositoryInMemory'
+import { CategoriesRepositoryInMemory } from '@modules/cars/repositories/in-memory/CategoriesRepositoryInMemory'
 import AppException from '@shared/exceptions/AppException'
 
-import { CreateCarUseCase } from './CreateCarUseCase'
+import { CreateCarUseCase, CreateCategoryUseCase } from '../index'
 
+let createCategoryUseCase: CreateCategoryUseCase
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory
 let createCarUseCase: CreateCarUseCase
 let carsRepositoryInMemory: CarsRepositoryInMemory
+let category: Category
 
 describe('Create Car', () => {
-  beforeEach(() => {
+  beforeEach(async () => {
     carsRepositoryInMemory = new CarsRepositoryInMemory()
-    createCarUseCase = new CreateCarUseCase(carsRepositoryInMemory)
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory()
+    createCarUseCase = new CreateCarUseCase(
+      carsRepositoryInMemory,
+      categoriesRepositoryInMemory,
+    )
+    createCategoryUseCase = new CreateCategoryUseCase(
+      categoriesRepositoryInMemory,
+    )
+    category = await createCategoryUseCase.execute({
+      name: 'valid_category',
+      description: 'valid_description',
+    })
   })
 
   test('should be able to register a new car', async () => {
@@ -20,7 +36,7 @@ describe('Create Car', () => {
       license_plate: 'valid_plate_one',
       fine_amount: 60,
       brand: 'valid_brand_one',
-      category_id: 'valid_category_one',
+      category_id: `${category.id}`,
     })
 
     expect(car).toHaveProperty('id')
@@ -35,30 +51,30 @@ describe('Create Car', () => {
         license_plate: 'valid_plate_two',
         fine_amount: 60,
         brand: 'valid_brand_two',
-        category_id: 'valid_category_two',
+        category_id: `${category.id}`,
       })
 
       await createCarUseCase.execute({
-        name: 'valid_name_two',
-        description: 'valid_description_two',
+        name: 'valid_name_three',
+        description: 'valid_description_three',
         daily_rate: 100,
-        license_plate: 'valid_plate_two',
+        license_plate: 'valid_plate_three',
         fine_amount: 60,
-        brand: 'valid_brand_two',
-        category_id: 'valid_category_two',
+        brand: 'valid_brand_three',
+        category_id: `${category.id}`,
       })
     }).rejects.toBeInstanceOf(AppException)
   })
 
   test('should be able to register a car as available by default', async () => {
     const car = await createCarUseCase.execute({
-      name: 'valid_name_three',
-      description: 'valid_description_three',
+      name: 'valid_name_four',
+      description: 'valid_description_four',
       daily_rate: 100,
-      license_plate: 'valid_plate_three',
+      license_plate: 'valid_plate_four',
       fine_amount: 60,
-      brand: 'valid_brand_three',
-      category_id: 'valid_category_three',
+      brand: 'valid_brand_four',
+      category_id: `${category.id}`,
     })
 
     expect(car.available).toBe(true)
