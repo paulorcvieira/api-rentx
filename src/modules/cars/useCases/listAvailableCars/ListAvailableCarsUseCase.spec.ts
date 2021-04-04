c3713454e597232419622f60e8f2a6ac7d6ed010
Paulo Vieira <paullocostta86@gmail.com>
2021-04-03 21:50:34 -0300

    :white_check_mark: test: only refactor test

--- src/modules/cars/useCases/listAvailableCars/ListAvailableCarsUseCase.spec.ts
+++ src/modules/cars/useCases/listAvailableCars/ListAvailableCarsUseCase.spec.ts
@@ -1,49 +1,30 @@
-import { Category } from '@modules/cars/infra/typeorm/entities/Category'
+import { v4 as uuidV4 } from 'uuid'
+
 import { CarsRepositoryInMemory } from '@modules/cars/repositories/in-memory/CarsRepositoryInMemory'
-import { CategoriesRepositoryInMemory } from '@modules/cars/repositories/in-memory/CategoriesRepositoryInMemory'
 
-import {
-  CreateCarUseCase,
-  ListAvailableCarsUseCase,
-  CreateCategoryUseCase,
-} from '../index'
+import { ListAvailableCarsUseCase } from '../index'
 
-let createCarUseCase: CreateCarUseCase
 let listAvailableCarsUseCase: ListAvailableCarsUseCase
 let carsRepositoryInMemory: CarsRepositoryInMemory
-let categoriesRepositoryInMemory: CategoriesRepositoryInMemory
-let createCategoryUseCase: CreateCategoryUseCase
-let category: Category
 
 describe('List Cars', () => {
   beforeEach(async () => {
     carsRepositoryInMemory = new CarsRepositoryInMemory()
-    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory()
-    createCarUseCase = new CreateCarUseCase(
-      carsRepositoryInMemory,
-      categoriesRepositoryInMemory,
-    )
     listAvailableCarsUseCase = new ListAvailableCarsUseCase(
       carsRepositoryInMemory,
     )
-    createCategoryUseCase = new CreateCategoryUseCase(
-      categoriesRepositoryInMemory,
-    )
-    category = await createCategoryUseCase.execute({
-      name: 'valid_category',
-      description: 'valid_description',
-    })
   })
 
   test('should be able to list all available cars', async () => {
-    const car = await createCarUseCase.execute({
+    const car = await carsRepositoryInMemory.create({
+      id: uuidV4(),
       name: 'valid_name_one',
       description: 'valid_description_one',
       daily_rate: 100,
       license_plate: 'valid_plate_one',
       fine_amount: 60,
       brand: 'valid_brand_one',
-      category_id: `${category.id}`,
+      category_id: 'valid_category_id',
     })
 
     const cars = await listAvailableCarsUseCase.execute({})
@@ -52,14 +33,15 @@ describe('List Cars', () => {
   })
 
   test('should be able to list all available cars by brand', async () => {
-    const car = await createCarUseCase.execute({
+    const car = await carsRepositoryInMemory.create({
+      id: uuidV4(),
       name: 'valid_name_two',
       description: 'valid_description_two',
       daily_rate: 100,
       license_plate: 'valid_plate_two',
       fine_amount: 60,
       brand: 'valid_brand_two',
-      category_id: `${category.id}`,
+      category_id: 'valid_category_id',
     })
 
     const cars = await listAvailableCarsUseCase.execute({
@@ -70,14 +52,15 @@ describe('List Cars', () => {
   })
 
   test('should be able to list all available cars by name', async () => {
-    const car = await createCarUseCase.execute({
+    const car = await carsRepositoryInMemory.create({
+      id: uuidV4(),
       name: 'valid_name_three',
       description: 'valid_description_three',
       daily_rate: 100,
       license_plate: 'valid_plate_three',
       fine_amount: 60,
       brand: 'valid_brand_three',
-      category_id: `${category.id}`,
+      category_id: 'valid_category_id',
     })
 
     const cars = await listAvailableCarsUseCase.execute({
@@ -88,18 +71,19 @@ describe('List Cars', () => {
   })
 
   test('should be able to list all available cars by category', async () => {
-    const car = await createCarUseCase.execute({
+    const car = await carsRepositoryInMemory.create({
+      id: uuidV4(),
       name: 'valid_name_four',
       description: 'valid_description_four',
       daily_rate: 100,
       license_plate: 'valid_plate_four',
       fine_amount: 60,
       brand: 'valid_brand_four',
-      category_id: `${category.id}`,
+      category_id: 'valid_category_id',
     })
 
     const cars = await listAvailableCarsUseCase.execute({
-      category_id: `${category.id}`,
+      category_id: 'valid_category_id',
     })
 
     expect(cars).toEqual([car])
