d6f28521662c9f5567135201882309edd5449c95
Paulo Vieira <paullocostta86@gmail.com>
2021-04-05 20:47:49 -0300

    :bug: fix: fix problem of returned value on delete

--- src/modules/cars/repositories/ICarsImagesRepository.ts
+++ src/modules/cars/repositories/ICarsImagesRepository.ts
@@ -2,7 +2,7 @@ import { CarImage } from '../infra/typeorm/entities/CarImage'
 
 interface ICarsImagesRepository {
   create(car_id: string, images_name: string): Promise<CarImage>
-  delete(images_id: string[]): Promise<CarImage[]>
+  delete(images_id: string[]): Promise<CarImage[] | void>
   list(car_id: string): Promise<CarImage[]>
 }
 
