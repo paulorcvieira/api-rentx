d191a3a128490d4085cfb0705b877e178c593287
Paulo Vieira <paullocostta86@gmail.com>
2021-04-05 21:10:56 -0300

    :bug: fix: insert injectable dependencies

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,4 +1,5 @@
 import { StatusCodes } from 'http-status-codes'
+import { inject, injectable } from 'tsyringe'
 
 import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental'
 import { IRentalsRepository } from '@modules/rentals/repositories/IRentalsRepository'
@@ -11,9 +12,13 @@ interface IRequest {
   expected_return_date: Date
 }
 
+@injectable()
 class CreateRentalUseCase {
   constructor(
+    @inject('RentalsRepository')
     private rentalsRepository: IRentalsRepository,
+
+    @inject('DateProvider')
     private dateProvider: IDateProvider,
   ) {}
 
