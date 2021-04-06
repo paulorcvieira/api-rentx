8e993bc0bd7c2d9b105760597289d44bee758f37
Paulo Vieira <paullocostta86@gmail.com>
2021-04-06 01:54:43 -0300

    :sparkles: chore: add supertest and configure

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,33 @@
+import 'reflect-metadata'
+
+import cors from 'cors'
+import express from 'express'
+import 'express-async-errors'
+import swaggerUi from 'swagger-ui-express'
+
+import uploadConfig from '@config/upload'
+import createConnection from '@shared/infra/typeorm'
+
+import generalException from './middlewares/generalException'
+import { routes } from './routes'
+import swaggerFile from './swagger.json'
+
+import '../../container'
+
+createConnection()
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.json())
+
+app.use('/files', express.static(uploadConfig.folder('tmp')))
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
+app.use(routes)
+
+app.use(generalException)
+
+export { app }
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,34 +1,9 @@
-import 'reflect-metadata'
 import 'dotenv/config'
 
-import cors from 'cors'
-import express from 'express'
-import 'express-async-errors'
-import swaggerUi from 'swagger-ui-express'
-
-import uploadConfig from '@config/upload'
-import createConnection from '@shared/infra/typeorm'
-
-import generalException from './middlewares/generalException'
-import { routes } from './routes'
-import swaggerFile from './swagger.json'
-
-import '../../container'
+import { app } from './app'
 
 const HOST = process.env.SERVER_HOST || 'http://localhost'
 const PORT = process.env.SERVER_PORT || 3333
-
-createConnection()
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-app.use('/files', express.static(uploadConfig.folder('tmp')))
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
-
-app.use(routes)
-
-app.use(generalException)
 
 app.listen(3333, () => {
   console.log(`🚀 Server started at ${HOST}:${PORT}`)
