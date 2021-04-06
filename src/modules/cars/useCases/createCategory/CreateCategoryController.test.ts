9b5464b0d368af01b1bb41c3d9bc4fadc2f6e923
Paulo Vieira <paullocostta86@gmail.com>
2021-04-06 02:00:38 -0300

    :white_check_mark: test: ci to ensure not be able to create a new category with name exists

--- src/modules/cars/useCases/createCategory/CreateCategoryController.test.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.test.ts
@@ -68,4 +68,24 @@ describe('Create Category Controller', () => {
 
     expect(response.status).toBe(201)
   })
+
+  test('should not be able to create a new category with name exists', async () => {
+    const responseToken = await request(app)
+      .post('/sessions')
+      .send({ emailOrUsername: 'admin@admin.com', password: 'admin' })
+
+    const { token } = responseToken.body
+
+    const response = await request(app)
+      .post('/categories')
+      .send({
+        name: 'Category Supertest',
+        description: 'Category Supertest',
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      })
+
+    expect(response.status).toBe(404)
+  })
 })
