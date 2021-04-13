a6cabb05e28eb6bfe7ea8e73db32ace02a7c8a28
Paulo Vieira <paullocostta86@gmail.com>
2021-04-13 18:28:44 -0300

    refactor: increase token expiration time

--- src/config/auth-config.ts
+++ src/config/auth-config.ts
@@ -1,6 +1,6 @@
 export const jwt = {
   secret_token: 'dfaf1a4c7ccaae1883ec4635594ed6aa',
-  expires_in_token: '15m',
+  expires_in_token: '1d',
   secret_refresh_token: 'efgt1a4c7ccaae1883ec4635594ed6gh',
   expires_in_refresh_token: '30d',
   expires_refresh_token_days: 30,
